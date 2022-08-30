4cd7113e62e0bb09a3bf9cd423759b8331a09bc5
Hae Lee <haeyounglee212@gmail.com>
2022-08-29 23:46:45 -0400

    'updata'

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import createMenu from "./components/menu.js";
 
 const menu = document.getElementById("menu");
 const mainImg = document.getElementById("mainImg");
-
-
+const homeMain = document.querySelector(".home-main");
 
 mainImg.src = main;
 
-menu.addEventListener("click", createMenu);
+menu.addEventListener("click", () => {
+  homeMain.style.opacity = "0";
+  createMenu();
+});
