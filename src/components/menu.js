ee6f62ba13006a51a0c37d1b93288bd9fbc0e3e8
Hae Lee <haeyounglee212@gmail.com>
2022-08-29 22:47:00 -0400

    'update'

---
+++ src/components/menu.js
@@ -0,0 +1,39 @@
+const htmlInsert = `<div class='appetizers'>
+<h3>Appetizers</h3>
+<div class="app-detail">
+<p>Edamame..............................$5.50</p>
+<p>Steamed Soybean Pods Sprinkled w/ Sea Salt</p>
+</div>
+<div class="app-detail">
+<p>Agedashi Tofu..............................$8.99</p>
+<p>Lightly Deep Fried Tofu w/ Special Sauce</p>
+</div>
+<div class="app-detail">
+<p>Shumai..............................$5.50</p>
+<p>6 Japanese Style Steamed Dumplings</p>
+</div>
+<div class="app-detail">
+<p>Miso Soup..............................$3.30</p>
+<p>Japanese Soybean Paste Soup</p>
+</div>
+<div class="app-detail">
+<p>Spicy Kani Salad..............................$6.50</p>
+<p>Crab Meat, Masago, Cucumber, Seaweed Noodle w/ Spicy Mayo</p>
+</div>
+<div class="app-detail">
+<p>Soft Shell Crab..............................$9.99</p>
+<p>Lightly Deep Fried Soft Shell Crab w/ Special Sauce</p>
+</div>
+<div class="app-detail">
+<p>Tempura Appetizer..............................$5.50</p>
+<p>Lightly Deep Fried 2 pcs Shrimp & 2 pcs Vegetables</p>
+</div>
+</div>`;
+
+const createMenu = () => {
+  const div = document.createElement("div");
+  div.innerHTML = htmlInsert;
+  content.appendChild("div");
+};
+
+export default createMenu;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "normalize.css";
 import "../styles/main.css";
 import main from "./assets/main.jpg";
-import "./menu.html";
-import "./contact.html";
+import createMenu from "./components/menu.js";
 
+const menu = document.getElementById("menu");
 const mainImg = document.getElementById("mainImg");
+
 mainImg.src = main;
+
+menu.addEventListener("click", createMenu);
